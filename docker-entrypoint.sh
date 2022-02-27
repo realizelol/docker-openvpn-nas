14cf3d20abf63888bc56b915cd23339b81f53a38
realizelol <bsw.bsw@gmx.de>
2022-02-27 22:59:31 +0100

    Give it a try #28. #alpha

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-set -x
-set -e
+#set -x
+#set -e
 
 # noninteractive
-DEBIAN_FRONTEND=noninteractive
-export DEBIAN_FRONTEND=noninteractive
+#DEBIAN_FRONTEND=noninteractive
+#export DEBIAN_FRONTEND=noninteractive
 
 # upgrade all packages (do not upgrade config files!)
-apt-get -qq update
-apt-get -yqq -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" full-upgrade
+#apt-get -qq update
+#apt-get -yqq -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" full-upgrade
 
 # install cont-environment
-mkdir -p /etc/services.d/openvpn
-cat > /etc/services.d/openvpn/run << EOF
+#mkdir -p /etc/services.d/openvpn
+#cat > /etc/services.d/openvpn/run << EOF
 #!/usr/bin/with-contenv bash
+#
+#/usr/local/bin/openvpn --nodaemon --umask=0077 --pidfile=/var/run/openvpn.pid --logfile=/var/log/openvpn.log
+#EOF
 
-/usr/local/bin/openvpn --nodaemon --umask=0077 --pidfile=/var/run/openvpn.pid --logfile=/var/log/openvpn.log
-EOF
-
-echo "Running openvpn - Which: $(whereis openvpn)"
-exec "/usr/local/bin/openvpn --config /etc/openvpn/openvpn.conf --client-config-dir /etc/openvpn/ccd --crl-verify /etc/openvpn/crl.pem"
+#echo "Running openvpn - Which: $(whereis openvpn)"
+#exec "/usr/local/bin/openvpn --config /etc/openvpn/openvpn.conf --client-config-dir /etc/openvpn/ccd --crl-verify /etc/openvpn/crl.pem"
+/bin/bash
