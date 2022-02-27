95a629a7f4990590abcf0bf6a58f39a8b1847d34
realizelol <bsw.bsw@gmx.de>
2022-02-28 00:13:39 +0100

    Give it a try #39. #alpha

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,19 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update
 apt-get -yqq -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" full-upgrade
 
+# init easy-rsa pki if not exist
+if [[ ! -f /etc/openvpn/tls-crypt.key ]]; then
+  cp -pR /usr/share/easy-rsa /etc/easy-rsa && \
+  cd /etc/easy-rsa
+  ./easyrsa init-pki
+  ./easyrsa build-ca
+  ./easyrsa gen-dh
+  ./easyrsa build-server-full server
+  openvpn --genkey secret /etc/easy-rsa/pki/ta.key
+  ./easyrsa gen-crl
+  cp -rp /etc/easy-rsa/pki/{ca.crt,dh.pem,ta.key,crl.pem,issued,private} /etc/openvpn/server
+fi
+
 # install cont-environment
 mkdir -p /etc/services.d/openvpn
 cat > /etc/services.d/openvpn/run << EOF
@@ -19,4 +32,4 @@ cat > /etc/services.d/openvpn/run << EOF
 EOF
 
 echo "$(date +'%Y-%M-%d %H:%M:%S') - Running openvpn"
-exec /bin/bash -c "/usr/sbin/openvpn --config /etc/openvpn/openvpn.conf --client-config-dir /etc/openvpn/ccd --crl-verify /etc/openvpn/crl.pem"
+exec /bin/bash -c "/usr/sbin/openvpn --config /etc/openvpn/server.conf --client-config-dir /etc/openvpn/ccd --crl-verify /etc/openvpn/crl.pem"
